fd2e3411d1da3472d2b15f9bd6396586ffd51c8c
odiaz <odiaz.dw@gmail.com>
2018-05-05 18:30:11 -0600

    testing out ngrx store, decided to go for angular/redux.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: [ './app.component.css' ]
+} )
 export class AppComponent {
-  title = 'app';
+  title = 'Ngrx JWT Auth';
+  constructor ( ) {
+
+  }
 }
